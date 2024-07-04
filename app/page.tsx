2e2ee9bb75305338411b3c97194574ff87187f91
sahatas <sahatasnutlee@gmail.com>
2024-07-04 16:47:28 +0700

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    // <h1>Hello, Next.js!</h1>
+    <header>
+        <h1>Next.js</h1>
+        <p>Hello</p>
+    </header>
+  )
+}
+
+export default page
+
+// export default function Page() {
+//     return <h1>Hello, Next.js!</h1>
+// }
