933ea18f3bc85ceb07b4818ba4726bbc322a7691
SAHATAS LEEVATHANA <sahatasnutlee@gmail.com>
2024-08-17 09:01:01 +0700

    Responsive Layout

--- app/components/narbar/links/Links.tsx
+++ app/components/narbar/links/Links.tsx
@@ -1,45 +1,63 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Navlink from './navLink/Navlink'
 import styles from './Links.module.css'
 
-const Links = () => {
-    const links = [
-        {
-            title: 'Home',
-            path: '/',
-        },
-        {
-            title: 'About',
-            path: '/about',
-        },
-        {
-            title: 'Contact',
-            path: '/contact',
-        },
-        // {
-        //     title: 'Login',
-        //     path: '/login',
-        // }
-    ]
+interface Link {
+    title: string;
+    path: string;
+}
 
+const links: Link[] = [
+    {
+        title: 'Home',
+        path: '/',
+    },
+    {
+        title: 'About',
+        path: '/about',
+    },
+    {
+        title: 'Contact',
+        path: '/contact',
+    }
+]
+
+const Links = () => {
+    const [open, setOpen] = useState(false);
+
+    // Temporary
     const session = true;
     const isAdmin = true;
 
     return (
-        <div className={styles.links}>
+        <div>
+            <div className={styles.links}>
+                {
+                    links.map((
+                        link => (<Navlink item={link} key={link.title} />)
+                    ))
+                }
+                {
+                    session ? (
+                        <>
+                            {isAdmin && <Navlink item={{ title: 'Admin', path: '/admin' }} />}
+                            <button className={styles.logout}>Logout</button>
+                        </>
+                    ) : (
+                        <Navlink item={{ title: 'Login', path: '/login' }} />
+                    )
+                }
+            </div>
+            <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
             {
-                links.map((
-                    link => (<Navlink item={link} key={link.title}/>)
-                ))
-            }
-            {
-                session ? (
-                    <>
-                    {isAdmin && <Navlink item={{title: 'Admin', path: '/admin' }} />}
-                    <button className={styles.logout}>Logout</button>
-                    </>
-                ) : (
-                    <Navlink item={{title: 'Login', path: '/login'}} />
+                open && (
+                    <div className={styles.mobileLinks}>
+                        {
+                            links.map((link) => <Navlink item={link} key={link.title} />)
+                        }
+                    </div>
                 )
             }
         </div>
